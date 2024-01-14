078c9f35f565403b590b91db750be337f1b3332f
Mike Pottebaum <michael.pottebaum@cdw.com>
2024-01-13 20:03:36 -0600

    calc the freakin gym days already

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import { v4 as uuid } from "uuid";
 
 export const meta: MetaFunction = () => {
@@ -7,6 +12,89 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Get it, Brother" },
   ];
 };
+
+const CYCLE_LENGTH = 9;
+
+type GymDayType = "arms" | "legs" | undefined;
+
+interface GymDay {
+  id: string;
+  type: GymDayType;
+  date: number;
+}
+
+type GymDays = Record<GymDay["date"], GymDay>;
+
+function isInCycle(startCycleDate: Date, date: Date) {
+  const startNextCycleDate = new Date(
+    startCycleDate.getFullYear(),
+    startCycleDate.getMonth(),
+    startCycleDate.getDate() + CYCLE_LENGTH,
+  );
+  return date > startCycleDate && date < startNextCycleDate;
+}
+
+const gymDayTypes: GymDayType[] = [
+  "arms",
+  undefined,
+  "legs",
+  undefined,
+  "arms",
+  undefined,
+  "legs",
+  undefined,
+  undefined,
+];
+
+function buildGymDays(startDate: Date, today: Date) {
+  let startCycleDate = startDate;
+  while (!isInCycle(startCycleDate, today)) {
+    startCycleDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + CYCLE_LENGTH,
+    );
+  }
+  const startGymDays = startCycleDate;
+
+  const lastDateInMonth = new Date(
+    today.getFullYear(),
+    today.getMonth() + 1,
+    0,
+  );
+  let currentDate = startGymDays;
+  const gymDays: GymDays = {};
+  let cycleDate = 0;
+  while (currentDate <= lastDateInMonth) {
+    const gymDay = {
+      id: uuid(),
+      type: gymDayTypes[cycleDate],
+      date: currentDate.getDate(),
+    };
+    gymDays[gymDay.date] = gymDay;
+    currentDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate() + 1,
+    );
+    cycleDate++;
+    if (cycleDate >= CYCLE_LENGTH) cycleDate = 0;
+  }
+  return gymDays;
+}
+
+export function loader(args: LoaderFunctionArgs) {
+  const startDate = new Date().toUTCString();
+  const startDateConstructo = new Date(startDate);
+  const gymDays = buildGymDays(startDateConstructo, new Date());
+  return json({
+    user: {
+      name: "Mike",
+      startDate,
+    },
+    gymDays,
+  });
+}
 
 const weekDays = [
   { name: "sunday", abbrev: "s" },
@@ -33,8 +121,12 @@ const months = [
   "december",
 ];
 
-export default function Index() {
-  const today = new Date();
+interface CalendarDate {
+  id: string;
+  date: number | null;
+}
+
+function buildDates(today: Date): CalendarDate[] {
   const year = today.getFullYear();
   const month = today.getMonth();
   const firstDate = new Date(year, month, 1);
@@ -48,7 +140,14 @@ export default function Index() {
     id: uuid(),
     date: null,
   }));
-  const paddedDays = [...daysPad, ...days];
+  return [...daysPad, ...days];
+}
+
+export default function Index() {
+  const { user, gymDays } = useLoaderData<typeof loader>();
+  const today = new Date();
+  const month = today.getMonth();
+  const dates = buildDates(today);
   return (
     <main className="flex flex-col">
       <section>
@@ -57,13 +156,23 @@ export default function Index() {
         </header>
         <article className="grid grid-cols-7">
           {weekDays.map(({ abbrev, name }) => (
-            <h2 key={name} className="uppercase">
+            <h2 key={name} className="uppercase text-center p-2">
               {abbrev}
             </h2>
           ))}
-          {paddedDays.map(({ date, id }) => (
-            <p key={id}>{date}</p>
-          ))}
+          {dates.map(({ date, id }) => {
+            const isGymDay = date && gymDays[date] && gymDays[date].type;
+            return (
+              <div key={id}>
+                {isGymDay && (
+                  <div className="bg-red-600">
+                    <button className="p-2">{date}</button>
+                  </div>
+                )}
+                {!isGymDay && <p>{date}</p>}
+              </div>
+            );
+          })}
         </article>
       </section>
     </main>
