a6b92058d829e06071db277af72336f9e192f6cb
Mike Pottebaum <michael.pottebaum@cdw.com>
2024-01-13 18:18:19 -0600

    addie the tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,7 +21,10 @@ module.exports = {
   },
 
   // Base config
-  extends: ["eslint:recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:tailwindcss/recommended",
+  ],
 
   overrides: [
     // React
